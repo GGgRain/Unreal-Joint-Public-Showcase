e3e0bb5ff8ec6b1bbbc9e71987f450bcd9100f79
GGgRain <69423737+GGgRain@users.noreply.github.com>
2026-01-24 00:06:38 +0900

    Update build settings to use latest version
    Changed DefaultBuildSettings from V2 to Latest in both JointPublicShowcaseEditor and JointPublicShowcase_BuildTargetSample target files to ensure builds use the most recent settings.

--- Source/JointPublicShowcaseEditor.Target.cs
+++ Source/JointPublicShowcaseEditor.Target.cs
@@ -8,7 +8,7 @@ public class JointPublicShowcaseEditorTarget : TargetRules
 	public JointPublicShowcaseEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 		ExtraModuleNames.AddRange(new string[] { "JointPublicShowcase", "JointPublicShowcaseEditor" });
 	}
 }
